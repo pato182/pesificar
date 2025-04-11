f6d5e5dcafaa7848a8ee95e3e9d0c078f29a34a3
Muras Patricio <muras.patricio@tecnica7.edu.ar>
2025-04-11 05:04:16 -0300

    Pequeño bug arreglado

--- frontend/pesificar.js
+++ frontend/pesificar.js
@@ -19,7 +19,7 @@ async function obtenerDolar() {
 }
 
 async function convertirPrecio(element) {
-    if (noConvertir.some(palabraClave => element.innerHTML == palabraClave) || (element.classList.contains("match_subtitle") && element.innerHTML == '')) return
+    if (noConvertir.some(palabraClave => element.innerHTML.includes(palabraClave)) || (element.classList.contains("match_subtitle") && element.innerHTML == '')) return
 
     if (element.firstElementChild?.classList.contains("discount_block") || element.dataset.convertido === "true") return
 
