1beea7aa84a80d94eb7f0ace477bb1dc1e4ae4a0
Muras Patricio <muras.patricio@tecnica7.edu.ar>
2025-04-11 05:27:34 -0300

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# pesificar
+
+# Pesificar
+
 Una forma sencilla de transformar los precios de la tienda de Steam a dolar tarjeta usando DolarAPI.
+
+## ¿Cómo funciona?
+
+Utilizando [Millenium](https://steambrew.app/), que permite usar plugins y temas en el cliente de Steam, se cambian todos los precios en dólares a pesos argentinos con la ayuda de la API de [DolarAPI](https://dolarapi.com/).
+
+## ¿Cómo se instala?
+
+1. Asegurarse de tener instalado [Millenium](https://docs.steambrew.app/users/installing/)
+2. Descargar la [última version](https://github.com/pato182/pesificar/releases) de Pesificar.
+3. Descomprimir el archivo zip en la carpeta "plugins" ubicada en la raíz de Steam.
+4. Activar el plugin desde la configuracion de Millenium (Steam>Millenium>Plugins).
+5. Reiniciar Steam manualmente o con el boton Guardar Cambios de Millenium.
+
+## Ayudas
+Cualquiera que se sienta libre de colaborar es bienvenido. Este proyecto fue armado bastante rápido y sin muchos conocimientos de la herramienta Millenium, asi que cualquier mejora o feedback se agradece!
+
+## Contacto
+
+Cualquier duda, feedback, bug o lo que sea es bienvenido de enviarme un mensaje a mi discord: 0800.pato
+
+
+
---
+++ frontend/pesificar.js
@@ -0,0 +1,59 @@
+var precioDolar
+const clasesPrecio = ".discount_final_price, .discount_original_price, .StoreOriginalPrice, .game_area_dlc_price, .game_purchase_price, ._3j4dI1yA7cRfCvK8h406OB, ._3fFFsvII7Y2KXNLDk_krOW, .pk-LoKoNmmPK4GBiC9DR8, ._2WLaY5TxjBGVyuWe_6KS3N, .DOnsaVcV0Is-, .ywNldZ-YzEE-, .match_subtitle"
+const noConvertir = [
+    "ARS$",
+    "Demo gratuita",
+    "Free to Play",
+    "Gratuito",
+    "Próximamente"
+]
+
+async function obtenerDolar() {
+    try {
+        const response = await fetch('https://dolarapi.com/v1/dolares/tarjeta')
+        const data = await response.json()
+        precioDolar = data.venta
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function convertirPrecio(element) {
+    if (noConvertir.some(palabraClave => element.innerHTML == palabraClave) || (element.classList.contains("match_subtitle") && element.innerHTML == '')) return
+
+    if (element.firstElementChild?.classList.contains("discount_block") || element.dataset.convertido === "true") return
+
+    if (element.classList.contains("_2WLaY5TxjBGVyuWe_6KS3N")) {
+        await new Promise(r => setTimeout(r, 500))
+    }
+
+    let precioOriginal = parseFloat(element.innerHTML.replace(/[^\d.,]/g, ""))
+    let precioFinal = (precioOriginal * precioDolar).toLocaleString("ES-ar", {minimumFractionDigits:2, maximumFractionDigits: 2})
+    
+    element.innerHTML = `ARS$ ${precioFinal}`
+    element.dataset.convertido = "true"
+}
+
+async function main() {
+    await obtenerDolar()
+
+    document.querySelectorAll(clasesPrecio).forEach(convertirPrecio)
+
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(nodo => {
+                if (nodo.nodeType !== 1) return
+                const precios = nodo.querySelectorAll?.(clasesPrecio)
+                precios.forEach(convertirPrecio)
+            })
+        })
+    })
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    })
+
+}
+
+main()
